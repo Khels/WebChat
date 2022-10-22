2438e35963dab0c45bd5c6ff3643d276df8c5742
Denis Mahnyuk <denismahnuk@gmail.com>
2022-10-22 18:54:22 +0300

    Developed base chat page

---
+++ frontend/src/components/MessageItem.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  ownMessage: {
+    type: Boolean
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+  backgroundColor: {
+    type: String,
+    default: '#ffa13d'
+  }
+})
+
+const styleObject = computed(() => {
+  return {
+    float: props.ownMessage ? 'right' : 'left',
+    backgroundColor: props.backgroundColor,
+  }
+})
+</script>
+
+<template>
+  <div class="message p-3 my-1" :style="styleObject">
+    <div class="text-break">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .message {
+    color: white;
+    border-radius: 1rem;
+    width: 40vw;
+  }
+</style>
---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import MessageItem from '../components/MessageItem.vue'
+import ResizableTextarea from '../components/ResizableTextarea.vue'
+
+const message = ref("ksdg;lfamskld;fjbnlksfdjsadnbsdfnb;lsdf;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+const messages = ref([])
+
+function sendMessage() {
+  if (message.value) {
+    messages.value.push(message.value)
+    message.value = ""
+  }
+}
+</script>
+
+<template>
+  <div class="container-fluid vh-100">
+    <div class="row" v-for="msg in messages" :key="msg">
+      <div class="col">
+        <MessageItem :text="msg" :own-message="true"/>
+      </div>
+    </div>
+    <div class="row fixed-bottom p-4">
+      <div class="col">
+        <ResizableTextarea
+          placeholder="Напишите сообщение..."
+          v-model="message"
+        ></ResizableTextarea>
+      </div>
+      <div class="col-sm-auto">
+        <button @click="sendMessage">Отправить</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
